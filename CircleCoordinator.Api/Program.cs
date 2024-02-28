607ade3e373d2a1ca3a69fbeeec254ee568f0eb7
Chebanu <0alexchebanu@gmail.com>
2024-02-28 12:20:00 +0200

    modified program.cs

--- CircleCoordinator.Api/Program.cs
+++ CircleCoordinator.Api/Program.cs
@@ -1,32 +1,30 @@
+using CircleCoordinator.Domain.Commands;
+using CircleCoordinator.Domain.Databases;
+using Microsoft.EntityFrameworkCore;
+
+using CircleCoordinator.Domain;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddControllers();
+
+builder.Services
+        .AddDbContext<AppDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services
+        .AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<CreateCircleCoordinatorCommand>());
+
+
+builder.Services.AddDomainServices();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-
-var summaries = new[]
+if (app.Environment.IsDevelopment())
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-});
-
-app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.MapControllers();
+
+await app.RunAsync();
