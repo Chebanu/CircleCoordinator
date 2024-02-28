fd6ef98d49b0e1bf1c5f9bc9cae227fabb897bd7
Chebanu <0alexchebanu@gmail.com>
2024-02-28 12:17:17 +0200

    add domain models

---
+++ CircleCoordinator.Domain/Models/Algorithm/Circle.cs
@@ -0,0 +1,9 @@
+﻿namespace CircleCoordinator.Domain.Models.Algorithm;
+
+public class Circle
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public int Diameter { get; set; }
+    public string Color { get; set; }
+}
---
+++ CircleCoordinator.Domain/Models/Database/Coordinator.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CircleCoordinator.Domain.Models.Database;
+
+[Table("coordinator")]
+public class Coordinator
+{
+    [Column("id")]
+    public Guid Id { get; set; }
+    [Column("createdAt")]
+    public DateTimeOffset CreatedAt { get; set; }
+    [Column("x")]
+    public int X { get; set; }
+    [Column("y")]
+    public int Y { get; set; }
+    [Column("diameter")]
+    public int Diameter { get; set; }
+    [Column("color")]
+    public string Color { get; set; }
+}
