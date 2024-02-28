436244707270e41e31fe7194f6d9766a57f2d2d1
Chebanu <0alexchebanu@gmail.com>
2024-02-28 12:19:45 +0200

    add repository pattern

---
+++ CircleCoordinator.Domain/DomainServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using CircleCoordinator.Domain.Algorithm;
+using CircleCoordinator.Domain.Helpers;
+using CircleCoordinator.Domain.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CircleCoordinator.Domain;
+
+public static class DomainServiceCollectionExtensions
+{
+    public static IServiceCollection AddDomainServices(this IServiceCollection services)
+    {
+        return services.AddSingleton<IDateTimeProvider, DateTimeProvider>()
+                        .AddScoped<ICircleDrawer, CircleDrawer>()
+                        .AddScoped<ICoordinatorRepository, CoordinatorRepository>();
+    }
+}
---
+++ CircleCoordinator.Domain/Repositories/CoordinatorRepository.cs
@@ -0,0 +1,34 @@
+﻿using CircleCoordinator.Domain.Databases;
+using CircleCoordinator.Domain.Models.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace CircleCoordinator.Domain.Repositories;
+
+public interface ICoordinatorRepository
+{
+    Task<Coordinator> CreateCoordinates(Coordinator coordinator, CancellationToken cancellationToken = default);
+    Task<Coordinator> GetCoordinates(Guid id, CancellationToken cancellationToken = default);
+}
+
+public class CoordinatorRepository : ICoordinatorRepository
+{
+    private readonly AppDbContext _dbContext;
+
+    public CoordinatorRepository(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<Coordinator> CreateCoordinates(Coordinator coordinator, CancellationToken cancellationToken = default)
+    {
+        _ = await _dbContext.AddAsync(coordinator, cancellationToken);
+        _ = await _dbContext.SaveChangesAsync(cancellationToken);
+
+        return coordinator;
+    }
+
+    public Task<Coordinator> GetCoordinates(Guid id, CancellationToken cancellationToken = default)
+    {
+        return _dbContext.Coordinators.SingleOrDefaultAsync(x => x.Id == id, cancellationToken);
+    }
+}
