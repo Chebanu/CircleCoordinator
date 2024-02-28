53b431e0e66bdee622d4e9eb3a1098bd4e7cffbc
Chebanu <0alexchebanu@gmail.com>
2024-02-28 12:20:14 +0200

    add mediatR

---
+++ CircleCoordinator.Domain/Commands/CreateCircleCommand.cs
@@ -0,0 +1,63 @@
+﻿using CircleCoordinator.Domain.Algorithm;
+using CircleCoordinator.Domain.Handlers;
+using CircleCoordinator.Domain.Helpers;
+using CircleCoordinator.Domain.Models.Database;
+using CircleCoordinator.Domain.Repositories;
+
+using MediatR;
+
+using Microsoft.Extensions.Logging;
+
+namespace CircleCoordinator.Domain.Commands;
+
+public class CreateCircleCoordinatorCommand : IRequest<CreateCircleCoordinatorResult>
+{
+    public int X { get; init; }
+    public int Y { get; init; }
+}
+
+public class CreateCircleCoordinatorResult
+{
+    public Guid Id { get; init; }
+    public string[] Errors { get; init; }
+    public bool Success { get; init; }
+}
+
+internal class CreateCircleCoordinatorCommandHandler :
+    BaseRequestHandler<CreateCircleCoordinatorCommand, CreateCircleCoordinatorResult>
+{
+    private readonly ICoordinatorRepository _repository;
+    private readonly ICircleDrawer _circleDrawer;
+    private readonly IDateTimeProvider _dateTimeProvider;
+
+    public CreateCircleCoordinatorCommandHandler(ICoordinatorRepository repository, ICircleDrawer circleDrawer, IDateTimeProvider dateTimeProvider, ILogger<BaseRequestHandler<CreateCircleCoordinatorCommand, CreateCircleCoordinatorResult>> logger) : base(logger)
+    {
+        _repository = repository;
+        _circleDrawer = circleDrawer;
+        _dateTimeProvider = dateTimeProvider;
+    }
+    protected override async Task<CreateCircleCoordinatorResult> HandleInternal(CreateCircleCoordinatorCommand request, CancellationToken cancellationToken)
+    {
+        var circleCoordinate = _circleDrawer.DrawCircle(request.X, request.Y);
+
+        var now = _dateTimeProvider.Now;
+
+        var drawnCircle = new Coordinator
+        {
+            Id = Guid.NewGuid(),
+            CreatedAt = now,
+            X = request.X,
+            Y = request.Y,
+            Diameter = circleCoordinate.Diameter,
+            Color = circleCoordinate.Color
+        };
+
+        _ = await _repository.CreateCoordinates(drawnCircle);
+
+        return new CreateCircleCoordinatorResult
+        {
+            Id = drawnCircle.Id,
+            Success = true
+        };
+    }
+}
---
+++ CircleCoordinator.Domain/Queries/GetCircleCoordinatorByIdQuery.cs
@@ -0,0 +1,37 @@
+﻿using CircleCoordinator.Domain.Handlers;
+using CircleCoordinator.Domain.Repositories;
+
+using MediatR;
+
+using Microsoft.Extensions.Logging;
+
+namespace CircleCoordinator.Domain.Queries;
+
+public class GetCircleCoordinatorByIdQuery : IRequest<GetCircleCoordinatorByIdResult>
+{
+    public Guid Id { get; init; }
+}
+
+public class GetCircleCoordinatorByIdResult
+{
+    public int X { get; init; }
+    public int Y { get; init; }
+    public int Diameter { get; init; }
+    public string Color { get; init; }
+    public string[] Errors { get; init; }
+}
+
+internal class GetCircleCoordinatorHandler : BaseRequestHandler<GetCircleCoordinatorByIdQuery, GetCircleCoordinatorByIdResult>
+{
+    private readonly ICoordinatorRepository _repository;
+
+    public GetCircleCoordinatorHandler(ICoordinatorRepository repository,
+        ILogger<BaseRequestHandler<GetCircleCoordinatorByIdQuery, GetCircleCoordinatorByIdResult>> logger) : base (logger)
+    {
+        _repository = repository;
+    }
+    protected override async Task<GetCircleCoordinatorByIdResult> HandleInternal(GetCircleCoordinatorByIdQuery request, CancellationToken cancellationToken)
+    {
+        return new GetCircleCoordinatorByIdResult();
+    }
+}
