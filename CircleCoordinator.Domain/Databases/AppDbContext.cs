06d2003d38b781c7bcec3b13f2e19e2d9325b0d8
Chebanu <0alexchebanu@gmail.com>
2024-02-28 12:18:51 +0200

    conneection to Db

---
+++ CircleCoordinator.Domain/Databases/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using CircleCoordinator.Domain.Models.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace CircleCoordinator.Domain.Databases;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Coordinator> Coordinators { get; init; }
+
+    public AppDbContext(DbContextOptions<AppDbContext> context) : base(context)
+    {
+    }
+}
---
+++ CircleCoordinator.Domain/Migrations/20240227235544_Initial.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CircleCoordinator.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "coordinator",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    createdAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    x = table.Column<int>(type: "integer", nullable: false),
+                    y = table.Column<int>(type: "integer", nullable: false),
+                    diameter = table.Column<int>(type: "integer", nullable: false),
+                    color = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_coordinator", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "coordinator");
+        }
+    }
+}
