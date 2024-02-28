5c89f9f5c6a53d634b81f5f9d44f7b1bc6e091de
Chebanu <0alexchebanu@gmail.com>
2024-02-28 12:17:37 +0200

    add algorithm for drawing circle

---
+++ CircleCoordinator.Domain/Algorithm/CircleDrawer.cs
@@ -0,0 +1,38 @@
+﻿using CircleCoordinator.Domain.Models.Algorithm;
+
+namespace CircleCoordinator.Domain.Algorithm;
+
+internal interface ICircleDrawer
+{
+    Circle DrawCircle(int x, int y);
+}
+internal class CircleDrawer : ICircleDrawer
+{
+    private Random random;
+
+    public CircleDrawer()
+    {
+        random = new Random();
+    }
+
+    public Circle DrawCircle(int x, int y)
+    {
+        int diameter = random.Next(10, 100); // Random diameter
+        int radius = diameter / 2;
+
+        // Calculate the top-left corner coordinates of the rectangle
+        int topLeftX = x - radius;
+        int topLeftY = y - radius;
+
+        // Generate random color
+        string randomColor = $"#{random.Next(0x1000000):X6}";
+
+        return new Circle
+        {
+            X = topLeftX,
+            Y = topLeftY,
+            Diameter = diameter,
+            Color = randomColor
+        };
+    }
+}
