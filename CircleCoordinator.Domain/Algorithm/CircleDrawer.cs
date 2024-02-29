8179c13e67afaec73cbd1beaab6fa5e8c5c10668
Chebanu <0alexchebanu@gmail.com>
2024-02-29 14:27:40 +0200

    add algorithm to calculate correct coordinates

--- CircleCoordinator.Domain/Algorithm/CircleDrawer.cs
+++ CircleCoordinator.Domain/Algorithm/CircleDrawer.cs
@@ -5,10 +5,14 @@ namespace CircleCoordinator.Domain.Algorithm;
 internal interface ICircleDrawer
 {
     Circle DrawCircle(int x, int y);
+    Circle DrawCircle(Circle circle);
 }
+
 internal class CircleDrawer : ICircleDrawer
 {
-    private Random random;
+    private readonly Random random;
+    private const int CanvasWidth = 200;
+    private const int CanvasHeight = 160;
 
     public CircleDrawer()
     {
@@ -17,14 +21,13 @@ internal class CircleDrawer : ICircleDrawer
 
     public Circle DrawCircle(int x, int y)
     {
-        int diameter = random.Next(10, 100); // Random diameter
+        int maxDiameter = Math.Min(CanvasWidth, CanvasHeight);
+        int diameter = random.Next(10, maxDiameter + 1);
         int radius = diameter / 2;
 
-        // Calculate the top-left corner coordinates of the rectangle
         int topLeftX = x - radius;
         int topLeftY = y - radius;
 
-        // Generate random color
         string randomColor = $"#{random.Next(0x1000000):X6}";
 
         return new Circle
@@ -33,6 +36,22 @@ internal class CircleDrawer : ICircleDrawer
             Y = topLeftY,
             Diameter = diameter,
             Color = randomColor
+        };
+    }
+
+    public Circle DrawCircle(Circle circle)
+    {
+        int radius = circle.Diameter / 2;
+
+        int topLeftX = circle.X - radius;
+        int topLeftY = circle.Y - radius;
+
+        return new Circle
+        {
+            X = topLeftX,
+            Y = topLeftY,
+            Diameter = circle.Diameter,
+            Color = circle.Color
         };
     }
 }
