d6cf19b710ed74c561ed8dbc5f38476920539fde
Chebanu <0alexchebanu@gmail.com>
2024-02-29 14:32:27 +0200

    add automapper customization

---
+++ CircleCoordinator.Domain/Profilies/AppProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using CircleCoordinator.Domain.Models.Database;
+
+namespace CircleCoordinator.Domain.Profilies;
+
+internal class AppProfile : Profile
+{
+    public AppProfile()
+    {
+        CreateMap<Coordinator, Contracts.Models.Coordinator>().ReverseMap();
+
+        CreateMap<Contracts.Models.CircleSet, CircleSet>();
+    }
+}
